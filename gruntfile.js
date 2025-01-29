86288f3e29a3a0c11d9ba7667acd4a5c4a7715d2
arnor <arnorsantosnunes01@gmail.com>
2025-01-29 20:01:55 -0300

    iniciando projeto grunt

--- gruntfile.js
+++ gruntfile.js
@@ -22,6 +22,10 @@ module.exports = function(grunt){
             less: {
                 files: ['src/styles/**/*.less'],
                 tasks: ['less:development']
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
             }
         },
         replace:{
@@ -31,6 +35,10 @@ module.exports = function(grunt){
                         {
                             match: 'ENDERECO_DO_CSS',
                                 replacement: './styles/main.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                                replacement: '../src/scripts/main.js'
                         }
                     ]
                 },
@@ -49,6 +57,10 @@ module.exports = function(grunt){
                         {
                             match: 'ENDERECO_DO_CSS',
                                 replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                                replacement: './scripts/main.min.js'
                         }
                     ]
                 },
@@ -73,14 +85,22 @@ module.exports = function(grunt){
                 }
             }
         },
-        clean: ['prebuild']
+        clean: ['prebuild'],
+        uglify: {
+            target:{
+                files:{
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
     })
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean']);
-}
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
+}             
